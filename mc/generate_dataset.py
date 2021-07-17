a3f94f854c55165e6036c6d0a3b22578f27741f6
infinityofspace <30715462+infinityofspace@users.noreply.github.com>
2021-07-17 21:22:42 +0200

    updated dataset generation script

--- mc/generate_dataset.py
+++ mc/generate_dataset.py
@@ -1,54 +1,183 @@
+import random
+from pathlib import Path
+
 import minerl
 import gym
-import cv2
-from pathlib import Path
-import numpy as np
 import imageio
+import numpy as np
 
-root_dataset_dir_path = Path("mc_predefinied_agent_actions")
+root_dataset_dir_path = Path("mc_predefinied_agent_actions_v3")
 
 env = gym.make("MineRLTreechop-v0")
 
+random_translation_actions = []
+for _ in range(200):
+    forward = random.randint(0, 1)
+    back = random.randint(0, 1) ^ forward
+    left = random.randint(0, 1)
+    right = random.randint(0, 1) ^ left
+    jump = random.randint(0, 1)
 
-# minerl-action-name, action-value, action-folder-name, number-of-samples, max-number-of-frames
+    random_translation_actions.extend([{
+        "forward": forward,
+        "back": back,
+        "left": left,
+        "right": right,
+        "jump": jump
+    }] * 3)
+
+random_rotation_actions = []
+for _ in range(200):
+    pitch = random.uniform(-90, 90)
+    yaw = random.uniform(-90, 90)
+
+    random_rotation_actions.extend([{
+        "pitch": pitch,
+        "yaw": yaw
+    }] * 3)
+
 dataset_actions = [
-    ("camera", np.array([0, 5]), "camera_yaw_5", 5, 73),
-    ("camera", np.array([0, 10]), "camera_yaw_10", 5, 37),
-    ("camera", np.array([0, 20]), "camera_yaw_20", 5, 19),
-    ("camera", np.array([0, 30]), "camera_yaw_30", 5, 13),
+    {
+        "actions": [
+                       {
+                           "camera": np.array([0, 5])
+                       }
+                   ] * 72 * 6,
+        "action_folder_name": "camera_yaw_5_repeated",
+        "samples": 5
+    },
+    {
+        "actions": [
+                       {
+                           "camera": np.array([0, 30])
+                       }
+                   ] * 12 * 40,
+        "action_folder_name": "camera_yaw_30_repeated",
+        "samples": 5
+    },
+    {
+        "actions": ([
+                        {
+                            "camera": np.array([5, 0])
+                        }
+                    ] * 18 +
+                    [
+                        {
+                            "camera": np.array([-5, 0])
+                        }
+                    ] * 18 * 2 +
+                    [
+                        {
+                            "camera": np.array([5, 0])
+                        }
+                    ] * 18) * 6,
+        "action_folder_name": "camera_pitch_5_repeated",
+        "samples": 5
+    },
+    {
+        "actions": ([
+                        {
+                            "camera": np.array([30, 0])
+                        }
+                    ] * 3 +
+                    [
+                        {
+                            "camera": np.array([-30, 0])
+                        }
+                    ] * 3 * 2 +
+                    [
+                        {
+                            "camera": np.array([30, 0])
+                        }
+                    ] * 3) * 40,
+        "action_folder_name": "camera_pitch_30_repeated",
+        "samples": 5
+    },
+    {
+        "actions": [
+                       {
+                           "forward": 1
+                       }
+                   ] * 500,
+        "action_folder_name": "forward",
+        "samples": 30
+    },
+    {
+        "actions": [
+                       {
+                           "back": 1
+                       }
+                   ] * 500,
+        "action_folder_name": "back",
+        "samples": 30
+    },
+    {
+        "actions": [
+                       {
+                           "left": 1
+                       }
+                   ] * 500,
+        "action_folder_name": "left",
+        "samples": 30
+    },
+    {
+        "actions": [
+                       {
+                           "right": 1
+                       }
+                   ] * 500,
+        "action_folder_name": "right",
+        "samples": 30
+    },
+    {
+        "actions": [
+                       {
+                           "jump": 1
+                       }
+                   ] * 500,
+        "action_folder_name": "jump",
+        "samples": 5
+    },
+    {
+        "actions": random_translation_actions,
+        "action_folder_name": "random_translation_3d",
+        "samples": 5
+    },
+    {
+        "actions": random_rotation_actions,
+        "action_folder_name": "random_rotation_3d",
+        "samples": 5
+    },
 ]
 
-
-for action_name, value, action_folder_name, n_samples, n_frames  in dataset_actions:
-    for sample in range(n_samples):
+for action_dict in dataset_actions:
+    for sample in range(action_dict["samples"]):
         obs = env.reset()
-        sample_dir_path = root_dataset_dir_path.joinpath(action_folder_name, str(sample))
+
+        sample_dir_path = root_dataset_dir_path.joinpath(action_dict["action_folder_name"], str(sample))
         sample_dir_path.mkdir(parents=True)
-        
+
         frames_dir_path = sample_dir_path.joinpath("frames")
         frames_dir_path.mkdir(parents=True)
-        
+
         sample_actions = []
-        
+
         i = 0
         action = env.action_space.noop()
-        
-        try:
 
+        try:
             obs, reward, done, info = env.step(action)
-            while i < n_frames and not done:
+            for act in action_dict["actions"]:
                 if imageio.imwrite(str(frames_dir_path.joinpath(str(i))) + ".png", obs["pov"]):
                     raise Exception("Could not save image")
-                                    
-                action[action_name] = value
+
+                action.update(act)
                 sample_actions.append(action)
-                
+
                 i += 1
-                
+
                 obs, reward, done, info = env.step(action)
         except RuntimeError as e:
             print(e)
         finally:
-            with open(str(sample_dir_path.joinpath("actions.npy")), "wb") as f:
-                np.save(f, np.array(sample_actions))
-
+            np.save(str(sample_dir_path.joinpath("actions.npy")), np.array(sample_actions))
